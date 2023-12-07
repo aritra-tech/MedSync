391d137b4272fcd10d84d235fb8a5388b11a271e
Aritra Das <aritrarick2002@gmail.com>
2023-12-07 21:44:34 +0530

    Added Top App Bar ✔

---
+++ app/src/main/java/com/aritra/medsync/components/CustomTopAppBar.kt
@@ -0,0 +1,54 @@
+package com.aritra.medsync.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.aritra.medsync.ui.theme.BackgroundDark
+import com.aritra.medsync.ui.theme.OnBackgroundDark
+import com.aritra.medsync.ui.theme.normal22
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopAppBar(
+    title: String,
+    shouldShowBackButton: Boolean = true,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(BackgroundDark), onBackPress: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = normal22,
+                color = OnBackgroundDark
+            )
+        },
+        navigationIcon = {
+            if (shouldShowBackButton) {
+                IconButton(onClick = {
+                    onBackPress()
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
+        colors = colors
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CustomTopAppBarPreview() {
+    CustomTopAppBar("Title goes here") {}
+}
