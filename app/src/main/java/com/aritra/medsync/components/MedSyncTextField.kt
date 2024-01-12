bf1354257c3f2647ac8a9322bd3ab4c8dcf0da8d
aritra <aritrarick2002@gmail.com>
2024-01-13 01:56:43 +0530

    🎨 Added `clip` to TextField

--- app/src/main/java/com/aritra/medsync/components/MedSyncTextField.kt
+++ app/src/main/java/com/aritra/medsync/components/MedSyncTextField.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -98,7 +100,7 @@ fun TextEditField(
     onValueChange: (String) -> Unit
 ) {
     TextField(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(15.dp)),
         value = value,
         keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
         onValueChange = {
