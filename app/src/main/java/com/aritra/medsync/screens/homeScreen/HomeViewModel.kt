543c76a8afb2129f9793d915a9d00afc53c7df65
aritra <aritrarick2002@gmail.com>
2024-01-13 20:29:05 +0530

    ✅ Changed viewmodel scope from `main` to `IO`

--- app/src/main/java/com/aritra/medsync/screens/homeScreen/HomeViewModel.kt
+++ app/src/main/java/com/aritra/medsync/screens/homeScreen/HomeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.aritra.medsync.domain.extensions.runIO
 import com.aritra.medsync.domain.model.Medication
 import com.aritra.medsync.screens.homeScreen.usecase.FetchMedicationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,15 +15,13 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val fetchMedicationUseCase: FetchMedicationUseCase
-): ViewModel() {
+) : ViewModel() {
 
     var medicationModel by mutableStateOf(emptyList<Medication>())
 
-    fun getMedications() {
-        viewModelScope.launch {
-            fetchMedicationUseCase.getAllMedications().collect { response ->
-                medicationModel = response
-            }
+    fun getMedications() = runIO {
+        fetchMedicationUseCase.getAllMedications().collect { response ->
+            medicationModel = response
         }
     }
 }
