813e1e93020a9c1e294bdf506c846813fc4e4f92
aritra <aritrarick2002@gmail.com>
2024-01-20 16:39:59 +0530

    ✅ Added func when clicked on the `buttons` on Medication Card

--- app/src/main/java/com/aritra/medsync/components/MedicationCard.kt
+++ app/src/main/java/com/aritra/medsync/components/MedicationCard.kt
@@ -1,5 +1,6 @@
 package com.aritra.medsync.components
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -21,37 +22,52 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.aritra.medsync.R
 import com.aritra.medsync.domain.model.Medication
+import com.aritra.medsync.screens.homeScreen.MedicationDetailsViewModel
 import com.aritra.medsync.ui.theme.Green
 import com.aritra.medsync.ui.theme.MedicineCircleColor
 import com.aritra.medsync.ui.theme.OnPrimaryContainer
 import com.aritra.medsync.ui.theme.OnSurface60
-import com.aritra.medsync.ui.theme.bold18
 import com.aritra.medsync.ui.theme.bold24
 import com.aritra.medsync.ui.theme.dividerColor
 import com.aritra.medsync.ui.theme.lightGreen
 import com.aritra.medsync.ui.theme.lightRed
-import com.aritra.medsync.ui.theme.medium14
 import com.aritra.medsync.ui.theme.medium16
-import com.aritra.medsync.ui.theme.medium18
 import com.aritra.medsync.ui.theme.normal14
 import com.aritra.medsync.ui.theme.red
 import com.aritra.medsync.utils.Utils.getMedicineUnit
 import com.aritra.medsync.utils.hasPassed
+import com.aritra.medsync.utils.onClick
 import com.aritra.medsync.utils.toFormattedTimeString
 
 @Composable
 fun MedicationCard(
     medication: Medication
 ) {
+
+    val updateMedicationViewModel: MedicationDetailsViewModel = hiltViewModel()
+    var isTakenClicked by remember {
+        mutableStateOf(medication.isTaken)
+    }
+    var isSkippedClicked by remember {
+        mutableStateOf(medication.isTaken.not())
+    }
+    val context = LocalContext.current
+
     Card(
         modifier = Modifier
             .fillMaxWidth(),
@@ -112,7 +128,7 @@ fun MedicationCard(
                         style = normal14.copy(color = OnSurface60)
                     )
                 }
-                
+
                 Spacer(modifier = Modifier.width(20.dp))
 
                 if (medication.reminderTime.hasPassed()) {
@@ -124,7 +140,19 @@ fun MedicationCard(
                             .padding(8.dp)
                     ) {
                         Icon(
-                            modifier = Modifier.size(25.dp),
+                            modifier = Modifier
+                                .size(25.dp)
+                                .onClick {
+                                    isTakenClicked = isTakenClicked.not()
+                                    if (isTakenClicked) {
+                                        isSkippedClicked = false
+                                    }
+                                    updateMedicationViewModel.isMedicationTaken(
+                                        medication,
+                                        isTakenClicked
+                                    )
+                                    Toast.makeText(context, "medication taken", Toast.LENGTH_SHORT).show()
+                                },
                             imageVector = Icons.Default.Check,
                             contentDescription = "Medication Taken",
                             tint = Green,
@@ -139,14 +167,22 @@ fun MedicationCard(
                             .padding(8.dp)
                     ) {
                         Icon(
-                            modifier = Modifier.size(25.dp),
+                            modifier = Modifier
+                                .size(25.dp)
+                                .onClick {
+                                    isSkippedClicked = isSkippedClicked.not()
+                                    if (isSkippedClicked) {
+                                        isTakenClicked = false
+                                    }
+                                    updateMedicationViewModel.isMedicationTaken(medication,isTakenClicked)
+                                    Toast.makeText(context, "medication not taken", Toast.LENGTH_SHORT).show()
+
+                                },
                             imageVector = Icons.Default.Clear,
                             contentDescription = "Medication Skipped",
                             tint = red,
                         )
                     }
-
-
                 }
             }
         }
