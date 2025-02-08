fd1e57e158cb5d88f3b922a861555421e4031cb3
aritra <aritrarick2002@gmail.com>
2025-02-08 09:21:49 +0530

    Fixed the edge to edge for History Screen

--- app/src/main/java/com/aritra/medsync/screens/history/HistoryScreen.kt
+++ app/src/main/java/com/aritra/medsync/screens/history/HistoryScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -34,9 +35,10 @@ fun HistoryScreen(
     Scaffold {
         Column(
             modifier = Modifier
-                .padding(it)
                 .fillMaxSize()
-                .background(PrimarySurface),
+                .background(PrimarySurface)
+                .padding(it)
+                .statusBarsPadding(),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             SortedMedicationList(medicationList)
