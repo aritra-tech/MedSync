a64601f718946627b8aba9b97385ce0a0a008f10
aritra <aritrarick2002@gmail.com>
2025-02-18 08:35:57 +0530

    Handled the notification permission efficiently

--- app/src/main/java/com/aritra/medsync/ui/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/aritra/medsync/ui/screens/homeScreen/HomeScreen.kt
@@ -1,6 +1,10 @@
 package com.aritra.medsync.ui.screens.homeScreen
 
 import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -21,9 +25,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.aritra.medsync.R
 import com.aritra.medsync.components.MedSyncEmptyState
 import com.aritra.medsync.components.MedSyncProgressCard
@@ -35,11 +45,8 @@ import com.aritra.medsync.ui.theme.PrimarySurface
 import com.aritra.medsync.ui.theme.bold22
 import com.aritra.medsync.ui.theme.medium24
 import com.aritra.medsync.utils.Utils
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberPermissionState
 
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun HomeScreen(
     userData: UserData?,
@@ -47,16 +54,36 @@ fun HomeScreen(
     navigateToUpdateScreen: (medicineID: Int) -> Unit,
     homeViewModel: HomeViewModel
 ) {
+    val context = LocalContext.current
     val medication = homeViewModel.medicationModel
     val greetingText = Utils.greetingText()
-    val postNotificationPermission = rememberPermissionState(Manifest.permission.POST_NOTIFICATIONS)
+    var hasNotificationPermission by remember {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            mutableStateOf(
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+        } else {
+            mutableStateOf(true)
+        }
+    }
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            hasNotificationPermission = isGranted
+        }
+    )
 
     LaunchedEffect(Unit) {
         homeViewModel.getMedications()
     }
 
     LaunchedEffect(Unit) {
-        postNotificationPermission.launchPermissionRequest()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
     }
 
     Scaffold(
