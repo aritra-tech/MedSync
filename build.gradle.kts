6f1e9ac8ab5f85584480b23e98955d5ca5dff0b8
aritra-tech <aritrarick2002@gmail.com>
2025-03-08 12:56:03 +0530

    fix: made some changes in the gradle files and updated the google services file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
     id ("com.google.dagger.hilt.android") version "2.49" apply false
